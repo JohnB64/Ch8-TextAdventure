e730676d8dbd3e140c6cbb1d40e5571fe4ae6d18
JohnB64 <47114060+JohnB64@users.noreply.github.com>
2019-04-22 12:41:35 -0400

    Started Exercise 11.8
    Used inheritance to make a subclass of Room, "TransporterRoom" and used the super keyword to get the String from the Room class and and let it be known to our TransporterRoom class

---
+++ TransporterRoom.java
@@ -0,0 +1,43 @@
+
+import java.util.Set;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * Chooses a random room and and sends the player to that room.
+ *
+ * @author John Burkert
+ * @version April 22nd 2019
+ */
+public class TransporterRoom extends Room
+{
+
+    /**
+     * Constructor for objects of class TransporterRoom
+     */
+    public TransporterRoom(String description) {
+        
+         super(description);
+        
+    }    
+    
+    /**
+     * Returns a random room
+     */
+  /**  public Room getExit(String direction)
+    {
+        return findRandomRoom();
+    }
+
+    /**
+     * Randomly chooses a random room
+     */
+   /** private Room findRandomRoom()
+    {
+        
+    }
+    
+    */
+}
