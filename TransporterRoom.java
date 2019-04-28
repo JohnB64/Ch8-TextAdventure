15f66980a839c0857cf0fc3e073e4d1db0a2759e
JohnB64 <47114060+JohnB64@users.noreply.github.com>
2019-04-27 22:44:43 -0400

    exercise 11.8
    Added all the room exits to choose from, just figuring out how to randomly pick one is the hard part

--- TransporterRoom.java
+++ TransporterRoom.java
@@ -13,14 +13,17 @@ import java.util.Random;
  */
 public class TransporterRoom extends Room
 {
+    
     private Room randomroom;
+    
     /**
      * Constructor for objects of class TransporterRoom
      */
     public TransporterRoom(String description) {
         
          super(description);
-         Random room = new Random();
+         
+         
     }    
     
     /**
@@ -36,7 +39,32 @@ public class TransporterRoom extends Room
      */
     private Room findRandomRoom()
     {
+        
+        Random room = new Random();
+        
+       // Room randomroom = room.nextInt();
 
+        randomroom = super.getExit("cell_door");
+        randomroom = super.getExit("prison_cell_2");
+        randomroom = super.getExit("prison_cell_3");
+        randomroom = super.getExit("prison_cell_4");
+        randomroom = super.getExit("your_cell");
+        randomroom = super.getExit("hanger_door");
+        randomroom = super.getExit("office_door");
+        randomroom = super.getExit("center_hallway");
+        randomroom = super.getExit("right_hallway");
+        randomroom = super.getExit("left_hallway");
+        randomroom = super.getExit("air_lock_door");
+        randomroom = super.getExit("storage_door");
+        randomroom = super.getExit("bathroom");
+        randomroom = super.getExit("spaceship_area");
+        randomroom = super.getExit("cargo_area");
+        randomroom = super.getExit("vehicle_area");
+        randomroom = super.getExit("space_ship");
+        randomroom = super.getExit("spaceship_cockpit");
+        
+        
+        
         return randomroom;
     }
     
